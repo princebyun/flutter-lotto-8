c0c6faac40c694fe20ec413f9219a869f6fe2b5e
princebyun <princebyun@gmail.com>
2025-11-16 23:51:53 +0900

    design : 구입 금액 입력 UI 생성

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,33 @@ class HomeScrean extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("로또 게임")),
-      body: Center(child: Text("로또 게임 구조 잡기")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '구입금액을 입력하세요.',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: '1000원 단위 금액만 숫자로 입력하세요.',
+              ),
+            ),
+            SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('구매하기'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(double.infinity, 50),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
