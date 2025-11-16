a45accc74efe436b19e7adaa4d88f8ccc4f654d5
princebyun <princebyun@gmail.com>
2025-11-17 02:05:57 +0900

    design : 결과 화면 번호 목록 UI 생성

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lotto_8/widgets/lotto_ball.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key});
@@ -7,7 +8,32 @@ class ResultScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('구입 결과')),
-      body: Center(child: Text('결과 화면')),
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('당첨번호', style: TextStyle(fontSize: 24)),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                LottoBall(number: 1),
+                LottoBall(number: 10),
+                LottoBall(number: 20),
+                LottoBall(number: 30),
+                LottoBall(number: 40),
+                LottoBall(number: 45),
+                Icon(Icons.add, size: 30),
+                LottoBall(number: 7),
+              ],
+            ),
+            Divider(height: 40, thickness: 1),
+            Text('내 구매 내역', style: TextStyle(fontSize: 24)),
+            SizedBox(height: 10),
+          ],
+        ),
+      ),
     );
   }
 }
