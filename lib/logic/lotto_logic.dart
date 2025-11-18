0c475740b4080777f98b8437f0fdf715ed97893e
princebyun <princebyun@gmail.com>
2025-11-18 23:12:56 +0900

    feat : 당첨 등수 판별 로직 구현

--- lib/logic/lotto_logic.dart
+++ lib/logic/lotto_logic.dart
@@ -3,6 +3,10 @@ import 'dart:math';
 
 void main() {
   print(createLotto());
+  List<int> winningLotto = [1, 2, 3, 4, 5, 6];
+  int bonusNumber = 17;
+  List<int> myLotto = [1, 2, 3, 4, 5, 17];
+  print(getWinner(winningLotto, bonusNumber, myLotto));
 }
 
 //로또 발금 메서드
@@ -17,4 +21,43 @@ List<int> createLotto() {
   lotto.sort();
 
   return lotto;
+}
+
+//당첨등수 판별 로직 구현
+String getWinner(List<int> winningLotto, int bonusNumber, List<int> myLotto) {
+  HashMap<String, int> result = new HashMap<String, int>();
+  int count = 0;
+  int bonus = 0;
+  for (var lotto in myLotto) {
+    if (winningLotto.contains(lotto)) {
+      count++;
+    }
+  }
+
+  if (myLotto.contains(bonusNumber)) {
+    bonus++;
+  }
+  result['count'] = count;
+  result['bonus'] = bonus;
+
+  return getWinnerGrade(count, bonus);
+}
+
+String getWinnerGrade(int count, int bonus) {
+  if (count == 6) {
+    return "1등";
+  }
+  if (count == 5 && bonus == 1) {
+    return "2등";
+  }
+  if (count == 5 && bonus == 0) {
+    return "3등";
+  }
+  if (count == 4) {
+    return "4등";
+  }
+  if (count == 3) {
+    return "5등";
+  }
+  return "꽝";
 }
